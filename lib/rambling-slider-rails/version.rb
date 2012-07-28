efae8ac82e1ac99bc21264b40906c6b88aeb5563
Edgar Gonzalez <edgar@ramblinglabs.com>
2012-07-28 13:56:40 -0400

    Bumped version to 0.1.1.

--- lib/rambling-slider-rails/version.rb
+++ lib/rambling-slider-rails/version.rb
@@ -2,7 +2,7 @@ module Rambling
   module Slider
     module Rails
       # Current version of rambling-slider-rails.
-      VERSION = '0.1.0'
+      VERSION = '0.1.1'
 
       # Current version of the jQuery Rambling Slider.
       JQUERY_RAMBLING_SLIDER_VERSION = '0.3.1'
